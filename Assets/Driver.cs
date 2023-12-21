99e9ae8190f08d1498d9c49102b83bc79af7a1a0
huseyindincc <huseyin.dinc1996@outlook.com>
2023-12-21 15:15:34 +0300

    day 2
    day 2 updates

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,6 +5,9 @@ using UnityEngine.EventSystems;
 
 public class Driver : MonoBehaviour
 {
+
+    float steerSpeed = 1f;
+    float moveSpeed = 0.01f;
     void Start()
     {
         
@@ -12,7 +15,7 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(0, 0.01f, 0);
-        transform.Rotate(0, 0, 0.1f);
+        transform.Translate(0, moveSpeed, 0);
+        transform.Rotate(0, 0, steerSpeed);
     }
 }
